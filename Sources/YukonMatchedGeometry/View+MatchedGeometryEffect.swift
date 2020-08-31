9e25d1a3d516ab408cb3583f508a1fa418e837ed
Andreas Tielmann <atielmann@deloitte.de>
2020-08-31 13:48:35 +0200

    Works with the Code 12 SDK

--- Sources/YukonMatchedGeometry/View+MatchedGeometryEffect.swift
+++ Sources/YukonMatchedGeometry/View+MatchedGeometryEffect.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension View {
-    public func matchedGeometryEffect<ID>(id: ID, in namespace: Namespace.ID, properties: MatchedGeometryProperties = .frame, anchor: UnitPoint = .center, isSource: Bool = true) -> some View where ID: Hashable {
+    public func matchedGeometryEffect<ID>(id: ID, in namespace: Namespace.ID, guid: String? = nil, properties: MatchedGeometryProperties = .frame, anchor: UnitPoint = .center, isSource: Bool = true) -> some View where ID: Hashable {
         let config = MatchedGeometryConfig(
             id: id,
             namespace: namespace,
@@ -17,5 +17,12 @@ extension View {
             isSource: isSource
         )
         return modifier(MatchedGeometryModifier(newConfig: config, oldConfig: config))
+    }
+}
+
+@available(iOS 14.0, *)
+extension View {
+    public func matchedGeometryEffect<ID>(id: ID, in namespace: SwiftUI.Namespace.ID, guid: String? = nil, properties: SwiftUI.MatchedGeometryProperties = .frame, anchor: UnitPoint = .center, isSource: Bool = true) -> some View where ID: Hashable {
+        matchedGeometryEffect(id: id, in: namespace, properties: properties, anchor: anchor, isSource: isSource)
     }
 }
